b9479d63b9843c7c2a3bd649e7f4fa7b58ee2703
nikolaitrifonov0 <nikolaitrifonov0@gmail.com>
2021-07-10 17:53:27 +0300

    Added exercises controller with add view.

---
+++ StayFit.Web/Controllers/ExercisesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StayFit.Web.Controllers
+{
+    public class ExercisesController : Controller
+    {
+        public IActionResult Add() => View();
+    }
+}
---
+++ StayFit.Web/Models/Exercises/AddExerciseFormModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StayFit.Web.Models.Exercises
+{
+    public class AddExerciseFormModel
+    {
+        public string Name { get; init; }
+        public string Description { get; init; }
+        public string Equipment { get; init; }
+        public string BodyPart { get; init; }
+    }
+}
